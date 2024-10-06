40bd42b75bf0fc54d846e5f8288d6d8fb0ab9cfc
Welf9i <welfkimedamine@gmail.com>
2024-10-06 17:30:48 +0100

    First Commit

---
+++ src/main/java/com/welfeki/demo/CoursApplication.java
@@ -0,0 +1,28 @@
+package com.welfeki.demo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import com.welfeki.demo.entities.Cours;
+import com.welfeki.demo.entities.Matiere;
+
+@SpringBootApplication
+public class CoursApplication implements CommandLineRunner {
+
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repositoryRestConfiguration.exposeIdsFor(Cours.class, Matiere.class);
+	}
+
+}
