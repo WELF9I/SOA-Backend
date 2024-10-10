766838673e4bae36bbc90e3b423504049d8cd7bb
Welf9i <welfkimedamine@gmail.com>
2024-10-10 19:06:08 +0100

    Delete old files

--- src/main/java/com/welfeki/demo/entities/CoursProjection.java
+++
@@ -1,8 +0,0 @@
-package com.welfeki.demo.entities;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "title", types = { Cours.class })
-public interface CoursProjection {
-    public String getTitle();
-}
